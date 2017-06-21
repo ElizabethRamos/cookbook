a34d52894906c1d40affc02dd7ac41b84e413a4b
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-06-21 19:01:10 -0300

    Adiciona receitas no arquivo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+Recipe.create(title: 'Bolo de cenoura', recipe_type: 'Sobremesa',
+                      cuisine: 'Brasileira', difficulty: 'Médio',
+                      cook_time: 60)
+
+Recipe.create(title: 'Feijoada', recipe_type: 'Prato Principal',
+                      cuisine: 'Brasileira', difficulty: 'Difícil',
+                      cook_time: 90)
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
