276d78aecd21d98eed6ab419551016bf26a3e77d
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-06-21 21:13:24 -0300

    Visitante visualiza detalhes de uma receita

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621233855) do
+ActiveRecord::Schema.define(version: 20170621234618) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170621233855) do
     t.integer "cook_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ingredients"
+    t.string "method"
   end
 
 end
