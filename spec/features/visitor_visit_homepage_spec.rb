2b3f46b636ac11d5b259c0bdca12b4af1ba3c4b3
Elizabeth <elizabethlopesramos@gmail.com>
2017-07-30 18:13:04 -0300

    Refatorando codigo com rubocop

--- spec/features/visitor_visit_homepage_spec.rb
+++ spec/features/visitor_visit_homepage_spec.rb
@@ -5,11 +5,12 @@ feature 'Visitor visit homepage' do
     visit root_path
 
     expect(page).to have_css('h1', text: 'CookBook')
-    expect(page).to have_css('p', text: 'Bem-vindo ao maior livro de receitas online')
+    expect(page).to have_css('p', text: 'Bem-vindo ao \
+    maior livro de receitas online')
   end
 
   scenario 'and view recipe' do
-    # cria os dados necessários
+    # cria os dados necessarios
     user = User.create(email: 'maria@campus.com', password: '12345678')
     cuisine = Cuisine.create(name: 'Brasileira')
     recipe_type = RecipeType.create(name: 'Sobremesa')
@@ -19,19 +20,19 @@ feature 'Visitor visit homepage' do
                            method: 'Misturar tudo, bater e assar',
                            cook_time: 60, user: user)
 
-    # simula a ação do usuário
+    # simula a acao do usuário
     visit root_path
 
-    # expectativas do usuário após a ação
+    # expectativas do usuario apos a acao
     expect(page).to have_css('h1', text: recipe.title)
     expect(page).to have_css('li', text: recipe.recipe_type.name)
     expect(page).to have_css('li', text: recipe.cuisine.name)
     expect(page).to have_css('li', text: recipe.difficulty)
-    expect(page).to have_css('li', text: "#{recipe.cook_time} minutos")
+    expect(page).to have_css('li', text: '#{recipe.cook_time} minutos')
   end
 
   scenario 'and view recipes list' do
-    # cria os dados necessários
+    # cria os dados necessarios
     user = User.create(email: 'maria@campus.com', password: '12345678')
     cuisine = Cuisine.create(name: 'Brasileira')
     recipe_type = RecipeType.create(name: 'Sobremesa')
@@ -48,20 +49,20 @@ feature 'Visitor visit homepage' do
                                    method: 'Cozinhar o feijao e refogar com as carnes já preparadas',
                                    cook_time: 90, user: user)
 
-    # simula a ação do usuário
+    # simula a acao do usuario
     visit root_path
 
-    # expectativas do usuário após a ação
+    # expectativas do usuario apos a acao
     expect(page).to have_css('h1', text: recipe.title)
     expect(page).to have_css('li', text: recipe.recipe_type.name)
     expect(page).to have_css('li', text: recipe.cuisine.name)
     expect(page).to have_css('li', text: recipe.difficulty)
-    expect(page).to have_css('li', text: "#{recipe.cook_time} minutos")
+    expect(page).to have_css('li', text: '#{recipe.cook_time} minutos')
 
     expect(page).to have_css('h1', text: another_recipe.title)
     expect(page).to have_css('li', text: another_recipe.recipe_type.name)
     expect(page).to have_css('li', text: another_recipe.cuisine.name)
     expect(page).to have_css('li', text: another_recipe.difficulty)
-    expect(page).to have_css('li', text: "#{another_recipe.cook_time} minutos")
+    expect(page).to have_css('li', text: '#{another_recipe.cook_time} minutos')
   end
 end
