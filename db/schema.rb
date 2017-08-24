7b8b9b4089ca83208e09e5af0ea5f84e3ef5f4da
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-08-23 22:40:46 -0300

    Transforma cook_time em integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725162420) do
+ActiveRecord::Schema.define(version: 20170824013906) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
