0da7fe5acbe38c701124dd499aedfe15766ddbfc
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-03-22 11:37:42 +0200

    Agrupa receitas por grau de dificuldade

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,17 +3,24 @@ class HomeController < ApplicationController
 
   def index
     @recipes = Recipe.last(6)
+    difficulty
   end
 
   def search
     @search = params[:search_text]
-    @recipes = Recipe.where('title = ?', params[:search_text])
+    @recipes = Recipe.where("title = ?", params[:search_text])
     render :search_results
   end
 
   def all_recipes
     @recipes = Recipe.all
     render 'index'
+  end
+
+  def difficulty
+    @easy_recipes = Recipe.where("difficulty = ?", "Fácil")
+    @medium_recipes = Recipe.where("difficulty = ?", "Médio")
+    @hard_recipes = Recipe.where("difficulty = ?", "Difícil")
   end
 
   private
