d736234e9fbd43104146fca51a3bd9cbba6489a8
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-03-29 12:58:34 +0300

    Adiciona rota para logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
   root to: 'home#index'
   get 'search', to: 'home#search'
   post 'search', to: 'home#search'
